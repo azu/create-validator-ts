6f2872c227f935858b3b69a8efc3114d51b60db7
Naoto Kato <naoto.kato@plaid.co.jp>
2021-05-06 12:54:38 +0900

    test: add extraTags test case

---
+++ test/snapshots/custom-generator-valid/custom-type-annotation.ts
@@ -0,0 +1,14 @@
+export type UpdateUser = {
+    /**
+     * @boolOption true
+     */
+    flg: string;
+    /**
+     * @strOption test
+     */
+    name: number;
+    /**
+     * @numberOption 123
+     */
+    age: number;
+};
