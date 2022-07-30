cda0b0d03b255276c69bce29cc7062d625d2e4c2
azu <azuciao@gmail.com>
2022-07-30 19:02:59 +0900

    feat: add --cache flag
    This flag cache the generated result.
    Make fast generating.

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import assert from "assert";
 import { generateValidator, TsJsonSchemaGeneratorOptions } from "./create-validator-ts";
 import { CodeGenerator } from "./default-code-generator";
 import path from "path";
+import { createCache } from "@file-cache/core";
+import { createNpmPackageKey } from "@file-cache/npm";
 
 export { GenerateValidatorCodeOptions, CodeGenerator } from "./default-code-generator";
 // TODO: Node 14+
@@ -15,6 +17,7 @@ export type CreateTSValidatorOptions = {
     tsconfigFilePath: string;
     codeGeneratorScript: string;
     targetGlobs: string[];
+    noCache?: boolean; // true by default
 } & TsJsonSchemaGeneratorOptions;
 
 export async function watchValidator(options: CreateTSValidatorOptions) {
@@ -129,8 +132,22 @@ export async function createValidator(options: CreateTSValidatorOptions) {
         absolute: true
     });
     const { sortProps, strictTuples, encodeRefs, skipTypeCheck, additionalProperties } = options;
+
+    const cache = await createCache({
+        keys: [() => createNpmPackageKey(["create-validator-ts"]), () => JSON.stringify(options)],
+        mode: "content",
+        // disable cache by default
+        noCache: options.noCache ?? true
+    });
     return Promise.all(
         files.map(async (filePath) => {
+            const cacheStatus = await cache.getAndUpdateCache(filePath);
+            if (!cacheStatus.changed) {
+                if (options.verbose) {
+                    console.log("Cached: " + filePath);
+                }
+                return;
+            }
             const result = await generateValidator({
                 cwd: options.cwd,
                 filePath: filePath,
@@ -151,5 +168,7 @@ export async function createValidator(options: CreateTSValidatorOptions) {
             }
             return fs.writeFile(result.validatorFilePath, result.code, "utf-8");
         })
-    );
+    ).finally(() => {
+        return cache.reconcile();
+    });
 }
